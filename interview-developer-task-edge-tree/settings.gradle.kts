697704edcb9040c744fa810e58d3d90e0eaaa8ce
pilulica <ibogdanovic994@gmail.com>
2025-07-07 18:02:52 +0200

    Add project via Spring Initializr

---
+++ interview-developer-task-edge-tree/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "interview-developer-task-edge-tree"
---
+++ interview-developer-task-edge-tree/src/main/kotlin/com/prewave/edgetree/EdgeTreeApiApplication.kt
@@ -0,0 +1,11 @@
+package com.prewave.edgetree
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EdgeTreeApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<EdgeTreeApiApplication>(*args)
+}
