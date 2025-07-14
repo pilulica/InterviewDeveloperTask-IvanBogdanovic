60c3fd1f17558af8f6fd47f6279bd3f1fbbfe7c4
pilulica <ibogdanovic994@gmail.com>
2025-07-14 16:27:22 +0200

    Add additional dependencies, properties and configuration(jooq)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import nu.studer.gradle.jooq.JooqEdition
+
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("nu.studer.jooq") version "8.2.1"
 }
 
 group = "com.prewave"
@@ -22,11 +25,23 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jooq:jooq:3.18.7")
+	implementation("org.jooq:jooq-kotlin:3.18.7")
+	implementation("org.springframework.boot:spring-boot-starter-jooq")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
 	runtimeOnly("org.postgresql:postgresql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	jooqGenerator("org.postgresql:postgresql")
+	jooqGenerator("org.jooq:jooq-meta-extensions:3.18.7")
 }
 
 kotlin {
@@ -37,4 +52,38 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+jooq {
+	version.set("3.18.7")
+	edition.set(JooqEdition.OSS)
+
+	configurations {
+		create("main") {
+			generateSchemaSourceOnCompilation.set(true)
+
+			jooqConfiguration.apply {
+				logging = org.jooq.meta.jaxb.Logging.WARN
+				jdbc.apply {
+					driver = "org.postgresql.Driver"
+					url = "jdbc:postgresql://localhost:5432/prewave_edge_tree_db"
+					user = "postgres"
+					password = "postgres123"
+				}
+				generator.apply {
+					name = "org.jooq.codegen.KotlinGenerator"
+					database.apply {
+						name = "org.jooq.meta.postgres.PostgresDatabase"
+						inputSchema = "edge_tree"
+						includes = "edge"
+						withIncludeForeignKeys(false)
+					}
+					target.apply {
+						packageName = "com.prewave.edgetree.generated"
+						directory = "build/generated-src/jooq/main"
+					}
+				}
+			}
+		}
+	}
 }
